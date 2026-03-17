06749ffb58c4bed2ef94bf13bc67cd6d4f3355a6
Ibtehaz Rafid <ir9269@rit.edu>
2026-03-17 18:02:51 -0400

    UserDAO start

--- src/main/java/com/moviedb/dao/UserDAO.java
+++ src/main/java/com/moviedb/dao/UserDAO.java
@@ -1,5 +1,18 @@
 package com.moviedb.dao;
+import java.sql.Connection;
 
 public class UserDAO {
+    public void createUser(Connection connect, String fName, String lName, String username, String email, String password) throws Exception {
+        String sql = "INSERT INTO users (first_name, last_name, username, email, password, last_access_date) VALUES (?, ?, ?, ?, ?, ?)";
+        try (var statement = connect.prepareStatement(sql)) {
+            statement.setString(1, fName);
+            statement.setString(2, lName);
+            statement.setString(3, username);
+            statement.setString(4, email);
+            statement.setString(5, password); //NEED TO HASH THIS
+            statement.executeUpdate();
+        }
+    }
+
     
 }
