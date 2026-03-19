466b029a3bbcded8cebdad0b8e37d59d38f7b933
nickli <linicholas16@gmail.com>
2026-03-18 23:18:36 -0400

    rate movie dao method done

--- src/main/java/com/moviedb/dao/MovieDAO.java
+++ src/main/java/com/moviedb/dao/MovieDAO.java
@@ -1,5 +1,29 @@
 package com.moviedb.dao;
 
+import java.sql.Connection;
+
 public class MovieDAO {
-    
+
+    // lets a user rate a movie from 1 to 5
+    // if they already rated it before, this just updates the old rating
+    public void rateMovie(Connection connect, int userID, int movieID, int starRating) throws Exception {
+        if (starRating < 1 || starRating > 5) {
+            throw new Exception("Rating must be between 1 and 5.");
+        }
+
+        String sql = """
+            INSERT INTO rates (user_id, movie_id, star_rating)
+            VALUES (?, ?, ?)
+            ON CONFLICT (user_id, movie_id)
+            DO UPDATE SET star_rating = EXCLUDED.star_rating
+            """;
+
+        try (var statement = connect.prepareStatement(sql)) {
+            statement.setInt(1, userID);
+            statement.setInt(2, movieID);
+            statement.setInt(3, starRating);
+
+            statement.executeUpdate();
+        }
+    }
 }
